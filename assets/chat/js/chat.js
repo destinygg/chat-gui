14f1531dda80dd02acbdba57d6c6298e9cf9d130
Farhad Jay <effejaye@gmail.com>
2020-09-03 23:22:18 -0700

    No longer display muted placeholder in DM windows
    Muted users can still send direct messages, so we should display the
    default placeholder text when a DM conversation window is active.

--- assets/chat/js/chat.js
+++ assets/chat/js/chat.js
@@ -298,10 +298,7 @@ class Chat {
             this.user = this.addUser(user)
             this.authenticated = true
         }
-        // TODO move this
-        if (this.authenticated) {
-            this.input.focus().attr('placeholder', `Write something ${this.user.username} ...`)
-        }
+        this.setDefaultPlaceholderText()
         return this
     }
 
@@ -481,7 +478,7 @@ class Chat {
         this.loadingscrn.fadeOut(250, () => this.loadingscrn.remove())
         this.mainwindow.updateAndPin()
 
-        this.input.focus().attr('placeholder', `Write something ...`)
+        this.setDefaultPlaceholderText()
         MessageBuilder.status(`Welcome to DGG chat`).into(this)
         return Promise.resolve(this)
     }
@@ -742,6 +739,13 @@ class Chat {
             if(win.locked()) win.unlock()
             this.redrawWindowIndicators()
         }
+
+        if (win.name === 'main' && this.mutedtimer.ticking) {
+            this.mutedtimer.updatePlaceholderText()
+        } else {
+            this.setDefaultPlaceholderText()
+        }
+
         return win
     }
 
@@ -844,6 +848,11 @@ class Chat {
         if(window.getSelection().isCollapsed && !this.input.is(':focus')) {
             this['debounceFocus'](this);
         }
+    }
+
+    setDefaultPlaceholderText() {
+        const placeholderText = this.authenticated ? `Write something ${this.user.username} ...` : `Write something ...`
+        this.input.attr('placeholder', placeholderText)
     }
 
     /**
