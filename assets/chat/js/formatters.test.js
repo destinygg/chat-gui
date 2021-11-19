0881ccc40de15c4ebb1b72c0420a55838f00926a
Farhad Jay <effejaye@gmail.com>
2021-11-18 20:49:56 -0800

    Create simple unit test to confirm things work

---
+++ assets/chat/js/formatters.test.js
@@ -0,0 +1,7 @@
+const getMessageForAboycalledsue = () => {
+    return 'aboycalledsue is lame'
+}
+
+test('Returns a message for aboycalledsue', () => {
+    expect(getMessageForAboycalledsue()).toBe('aboycalledsue is lame')
+})
