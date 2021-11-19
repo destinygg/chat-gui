61bdbc950660080a27113373b1e5d38d269799dd
Farhad Jay <effejaye@gmail.com>
2021-11-18 21:46:37 -0800

    Write first test for Amazon formatter

--- assets/chat/js/formatters.test.js
+++ assets/chat/js/formatters.test.js
@@ -1,7 +1,15 @@
-const getMessageForAboycalledsue = () => {
-    return 'aboycalledsue is lame'
+import { AmazonAssociatesTagInjector } from './formatters'
+
+const chatStub = {
+    config: {
+        amazonTag: 'cakesh-20'
+    }
 }
 
-test('Returns a message for aboycalledsue', () => {
-    expect(getMessageForAboycalledsue()).toBe('aboycalledsue is lame')
+test('Inject tag into Amazon link', () => {
+    const message = 'hey check out these new earphones i bought https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details pretty cool eh?'
+    const expectedMessage = 'hey check out these new earphones i bought https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details&tag=cakesh-20 pretty cool eh?'
+
+    const injector = new AmazonAssociatesTagInjector()
+    expect(injector.format(chatStub, message)).toBe(expectedMessage)
 })
