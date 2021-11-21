545237839cd0e4aa2c9626c6a1753a41a9de433d
Farhad Jay <effejaye@gmail.com>
2021-11-20 21:54:01 -0800

    Modify tests to handle all outcomes

--- assets/chat/js/formatters.test.js
+++ assets/chat/js/formatters.test.js
@@ -11,38 +11,63 @@ const chatStub = {
     }
 }
 
+const chatStubWithoutTags = {
+    config: {
+        amazonTags: null
+    }
+}
+
 test.each([
     [
-        'Inject tags into message with two links',
-        'hey check these out https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details and this too https://www.amazon.com/dp/B07VP5WG78?psc=1&ref=ppx_yo2_dt_b_product_details',
-        'hey check these out https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details&tag=cakesh-20 and this too https://www.amazon.com/dp/B07VP5WG78?psc=1&ref=ppx_yo2_dt_b_product_details&tag=cakesh-20',
-        null
+        'Inject tag into message',
+        'hey check these out https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
+        'hey check these out https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details&tag=cakesh-20',
+        null,
+        chatStub
     ],
     [
         'Inject tags into message with international links',
         'for the canadians https://www.amazon.ca/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details the british https://www.amazon.co.uk/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details and the germans https://www.amazon.de/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
         'for the canadians https://www.amazon.ca/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details&tag=leafsh-20 the british https://www.amazon.co.uk/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details&tag=bongsh-20 and the germans https://www.amazon.de/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details&tag=beersh-20',
-        null
+        null,
+        chatStub
+    ],
+    [
+        'Don\'t inject tag when no tag for country exists',
+        'welcome japan https://www.amazon.co.jp/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
+        'welcome japan https://www.amazon.co.jp/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
+        null,
+        chatStub
     ],
     [
         'Replace existing tag with new tag',
         'hey check these out https://www.amazon.com/dp/B08LMBJFGD?psc=1&tag=fakesh-20&ref=ppx_yo2_dt_b_product_details pretty cool eh?',
         'hey check these out https://www.amazon.com/dp/B08LMBJFGD?psc=1&tag=cakesh-20&ref=ppx_yo2_dt_b_product_details pretty cool eh?',
-        null
+        null,
+        chatStub
     ],
     [
         'Return original message if it becomes too long',
         'cool earphones check it out lads https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
         'cool earphones check it out lads https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
-        110
+        110,
+        chatStub
     ],
     [
-        'Don\'t inject tags into non-Amazon links',
-        'lol click this link to see something cool https://www.destiny.gg/logout',
-        'lol click this link to see something cool https://www.destiny.gg/logout',
-        null
+        'Return original message if URL malformed',
+        'hehehe nasty link incoming https://www.amazon.comF*()&*($^&)(#*^&$@#(()*&() hehe',
+        'hehehe nasty link incoming https://www.amazon.comF*()&*($^&)(#*^&$@#(()*&() hehe',
+        null,
+        chatStub
     ],
-])('%s', (_, message, expectedMessage, maxMessageSize) => {
+    [
+        'Return original message if no tags provided',
+        'cool earphones check it out lads https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
+        'cool earphones check it out lads https://www.amazon.com/dp/B08LMBJFGD?psc=1&ref=ppx_yo2_dt_b_product_details',
+        null,
+        chatStubWithoutTags
+    ],
+])('%s', (_, message, expectedMessage, maxMessageSize, chatStub) => {
     const injector = new AmazonAssociatesTagInjector(maxMessageSize)
     expect(injector.format(chatStub, message)).toBe(expectedMessage)
 })
