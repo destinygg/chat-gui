055d3c80542876005c47b38453b771f89ee4cbda
Farhad Jay <effejaye@gmail.com>
2022-11-07 17:59:58 -0800

    Restore original notification module
    This module is written by a third party.

--- assets/chat/js/notification.js
+++ assets/chat/js/notification.js
@@ -1,5 +1,4 @@
 /* eslint-disable */
-/* global window, document, navigator */
 /*
 Copyright (C) 2013 Hendrik Beskow
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -18,154 +17,111 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 const Notification = (function (win, doc, nav) {
-  if (
-    /* Safari 6, Firefox 22 */ !(
-      'Notification' in win &&
-      'permission' in win.Notification &&
-      'requestPermission' in win.Notification
-    )
-  ) {
-    const PERMISSION_DEFAULT = 'default';
-    const PERMISSION_GRANTED = 'granted';
-    const PERMISSION_DENIED = 'denied';
-    const PERMISSION = [
-      PERMISSION_GRANTED,
-      PERMISSION_DEFAULT,
-      PERMISSION_DENIED,
-    ];
-    const isString = function (value) {
-      return value && value.constructor === String;
-    };
-    const isFunction = function (value) {
-      return value && value.constructor === Function;
-    };
-    const isObject = function (value) {
-      return value && value.constructor === Object;
-    };
-    const noop = function () {};
-    const localStorage = window.localStorage || {
-      setItem: noop,
-      getItem: noop,
-    };
+    if ( /* Safari 6, Firefox 22 */ !('Notification' in win && 'permission' in win.Notification && 'requestPermission' in win.Notification)) {
+        const PERMISSION_DEFAULT = 'default',
+            PERMISSION_GRANTED = 'granted',
+            PERMISSION_DENIED = 'denied',
+            PERMISSION = [PERMISSION_GRANTED, PERMISSION_DEFAULT, PERMISSION_DENIED],
+            isString = function (value) {
+                return (value && (value).constructor === String);
+            },
+            isFunction = function (value) {
+                return (value && (value).constructor === Function);
+            },
+            isObject = function (value) {
+                return (value && (value).constructor === Object);
+            },
+            noop = function () {
+            };
+        const localStorage = window.localStorage || {
+            setItem: noop,
+            getItem: noop
+        };
 
-    const checkPermission = function () {
-      let permission;
-      if (
-        /* Chrome, Firefox < 22 && ff-html5notifications */ 'webkitNotifications' in
-          win &&
-        'checkPermission' in win.webkitNotifications
-      ) {
-        permission = PERMISSION[win.webkitNotifications.checkPermission()];
-      } else if (/* Firefox Mobile */ 'mozNotification' in nav) {
-        permission = PERMISSION_GRANTED;
-      } else {
-        permission =
-          localStorage.getItem('notifications') === null
-            ? PERMISSION_DEFAULT
-            : localStorage.getItem('notifications');
-      }
-      return permission;
-    };
-
-    const requestPermission = function (callback) {
-      const callbackFunction = isFunction(callback) ? callback : noop;
-      if (
-        /* Chrome, Firefox < 22 && ff-html5notifications */ 'webkitNotifications' in
-          win &&
-        'requestPermission' in win.webkitNotifications
-      ) {
-        win.webkitNotifications.requestPermission(callbackFunction);
-      } else {
-        if (checkPermission() === PERMISSION_DEFAULT) {
-          if (
-            confirm(
-              `Do you want to allow ${doc.domain} to display Notifications?`
-            )
-          ) {
-            localStorage.setItem('notifications', PERMISSION_GRANTED);
-            Notification.permission = PERMISSION_GRANTED;
-          } else {
-            localStorage.setItem('notifications', PERMISSION_DENIED);
-            Notification.permission = PERMISSION_DENIED;
-          }
+        const checkPermission = function () {
+            let permission;
+            if ( /* Chrome, Firefox < 22 && ff-html5notifications */ !!('webkitNotifications' in win && 'checkPermission' in win.webkitNotifications)) {
+                permission = PERMISSION[win.webkitNotifications.checkPermission()];
+            } else if ( /* Firefox Mobile */ 'mozNotification' in nav) {
+                permission = PERMISSION_GRANTED;
+            } else {
+                permission = (localStorage.getItem('notifications') === null) ? PERMISSION_DEFAULT : localStorage.getItem('notifications');
+            }
+            return permission;
         }
-        callbackFunction();
-      }
-    };
 
-    const Notification = function (title, params) {
-      let notification;
-      if (
-        isString(title) &&
-        isObject(params) &&
-        checkPermission() === PERMISSION_GRANTED
-      ) {
-        if (/* Firefox Mobile */ 'mozNotification' in nav) {
-          notification = nav.mozNotification.createNotification(
-            title,
-            params.body,
-            params.icon
-          );
-          if (isFunction(params.onclick)) {
-            notification.onclick = params.onclick;
-          }
-          if (isFunction(params.onclose)) {
-            notification.onclose = params.onclose;
-          }
-          notification.show();
-        } else if (
-          /* Chrome, Firefox < 22 && ff-html5notifications */ 'webkitNotifications' in
-            win &&
-          'createNotification' in win.webkitNotifications
-        ) {
-          notification = win.webkitNotifications.createNotification(
-            params.icon,
-            title,
-            params.body
-          );
-          if (isFunction(params.onclick)) {
-            notification.onclick = params.onclick;
-          }
-          if (isFunction(params.onshow)) {
-            notification.onshow = params.onshow;
-          }
-          if (isFunction(params.onerror)) {
-            notification.onerror = params.onerror;
-          }
-          if (isFunction(params.onclose)) {
-            notification.onclose = params.onclose;
-          }
-          notification.show();
+        const requestPermission = function (callback) {
+            let callbackFunction = isFunction(callback) ? callback : noop;
+            if ( /* Chrome, Firefox < 22 && ff-html5notifications */ !!('webkitNotifications' in win && 'requestPermission' in win.webkitNotifications)) {
+                win.webkitNotifications.requestPermission(callbackFunction);
+            } else {
+                if (checkPermission() === PERMISSION_DEFAULT) {
+                    if (confirm('Do you want to allow ' + doc.domain + ' to display Notifications?')) {
+                        localStorage.setItem('notifications', PERMISSION_GRANTED);
+                        Notification.permission = PERMISSION_GRANTED;
+                    } else {
+                        localStorage.setItem('notifications', PERMISSION_DENIED);
+                        Notification.permission = PERMISSION_DENIED;
+                    }
+                }
+                callbackFunction();
+            }
+        }
+
+        const Notification = function (title, params) {
+            let notification;
+            if (isString(title) && isObject(params) && checkPermission() === PERMISSION_GRANTED) {
+                if ( /* Firefox Mobile */ 'mozNotification' in nav) {
+                    notification = nav.mozNotification.createNotification(title, params.body, params.icon);
+                    if (isFunction(params.onclick)) {
+                        notification.onclick = params.onclick;
+                    }
+                    if (isFunction(params.onclose)) {
+                        notification.onclose = params.onclose;
+                    }
+                    notification.show();
+                } else if ( /* Chrome, Firefox < 22 && ff-html5notifications */ 'webkitNotifications' in win && 'createNotification' in win.webkitNotifications) {
+                    notification = win.webkitNotifications.createNotification(params.icon, title, params.body);
+                    if (isFunction(params.onclick)) {
+                        notification.onclick = params.onclick;
+                    }
+                    if (isFunction(params.onshow)) {
+                        notification.onshow = params.onshow;
+                    }
+                    if (isFunction(params.onerror)) {
+                        notification.onerror = params.onerror;
+                    }
+                    if (isFunction(params.onclose)) {
+                        notification.onclose = params.onclose;
+                    }
+                    notification.show();
+                } else {
+                    // Your custom code goes here
+                }
+            }
+            return notification;
+        }
+
+        if (!('Notification' in win)) {
+            Notification.requestPermission = requestPermission;
+            win.Notification = Notification;
+        }
+
+        if (!!('webkitNotifications' in win && 'checkPermission' in win.webkitNotifications)) {
+            Object.defineProperty(win.Notification, 'permission', {
+                get: function () {
+                    return PERMISSION[win.webkitNotifications.checkPermission()];
+                }
+            });
         } else {
-          // Your custom code goes here
+            Notification.permission = checkPermission();
         }
-      }
-      return notification;
-    };
 
-    if (!('Notification' in win)) {
-      Notification.requestPermission = requestPermission;
-      win.Notification = Notification;
+        if (!('requestPermission' in win.Notification)) {
+            win.Notification.requestPermission = requestPermission;
+        }
     }
-
-    if (
-      'webkitNotifications' in win &&
-      'checkPermission' in win.webkitNotifications
-    ) {
-      Object.defineProperty(win.Notification, 'permission', {
-        get() {
-          return PERMISSION[win.webkitNotifications.checkPermission()];
-        },
-      });
-    } else {
-      Notification.permission = checkPermission();
-    }
-
-    if (!('requestPermission' in win.Notification)) {
-      win.Notification.requestPermission = requestPermission;
-    }
-  }
-  return win.Notification;
-})(window, document, navigator);
+    return win.Notification
+}(window, document, navigator));
 
 export { Notification };
