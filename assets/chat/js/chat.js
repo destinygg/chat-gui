57c03008257d7622ea75d92455a22ee7c70fcea8
Farhad Jay <effejaye@gmail.com>
2022-12-18 17:07:48 -0800

    Simplify mentioned username regex

--- assets/chat/js/chat.js
+++ assets/chat/js/chat.js
@@ -30,7 +30,7 @@ import makeSafeForRegex from './regex';
 
 const regexslashcmd = /^\/([a-z0-9]+)[\s]?/i;
 const regextime = /(\d+(?:\.\d*)?)([a-z]+)?/gi;
-const nickmessageregex = /(?:(?:^|\s)@?)([a-zA-Z0-9_]{3,20})(?=$|\s|[.?!,])/g;
+const nickmessageregex = /@?(\w{3,20})/g;
 const nickregex = /^[a-zA-Z0-9_]{3,20}$/;
 const nsfwnsflregex = /\b(?:NSFL|NSFW)\b/i;
 const nsfwregex = /\b(?:NSFW)\b/i;
