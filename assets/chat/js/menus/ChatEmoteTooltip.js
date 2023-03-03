2c27b7f59b38f1f107fac07f4870a29ca3ac600d
Mitchdev <admin@mitchdev.net>
2023-03-03 17:52:08 +1300

    setters

--- assets/chat/js/menus/ChatEmoteTooltip.js
+++ assets/chat/js/menus/ChatEmoteTooltip.js
@@ -5,32 +5,26 @@ export default class ChatEmoteTooltip extends ChatMenuFloating {
   constructor(ui, btn, chat) {
     super(ui, btn, chat);
 
-    this.emote = this.ui.find('.emote-image');
-    this.name = this.ui.find('.emote-info .name');
-    this.creator = this.ui.find('.emote-info .creator');
-    this.tier = this.ui.find('.emote-info .tier');
+    this.ui.emote = this.ui.find('.emote-image');
+    this.ui.name = this.ui.find('.emote-info .name');
+    this.ui.creator = this.ui.find('.emote-info .creator');
+    this.ui.tier = this.ui.find('.emote-info .tier');
 
     this.chat.output.on('contextmenu', '.msg-chat .text .emote', (e) => {
       const emote = $(e.currentTarget).closest('.emote')[0].innerText;
       const { creator, minimumSubTier } =
         this.chat.emoteService.getEmote(emote) ?? {};
 
-      this.name.text(emote);
-
-      this.creator.text(creator);
-      this.creator[creator ? 'show' : 'hide']();
-
-      this.tier.text(`Tier ${minimumSubTier}`);
-      this.tier[minimumSubTier > 0 ? 'show' : 'hide']();
-
-      this.emote.html(`<div class="emote ${emote}">${emote}</div>`);
+      this.emote = emote;
+      this.creator = creator;
+      this.tier = minimumSubTier;
 
       this.position(e);
       this.show();
       return false;
     });
 
-    this.emote.on('click', '.emote', (e) => {
+    this.ui.emote.on('click', '.emote', (e) => {
       const value = this.chat.input.val().toString().trim();
       this.chat.input
         .val(
@@ -38,5 +32,29 @@ export default class ChatEmoteTooltip extends ChatMenuFloating {
         )
         .focus();
     });
+  }
+
+  /**
+   * @param {string} emote
+   */
+  set emote(emote) {
+    this.ui.name.text(emote);
+    this.ui.emote.html(`<div class="emote ${emote}">${emote}</div>`);
+  }
+
+  /**
+   * @param {string} creator
+   */
+  set creator(creator) {
+    this.ui.creator.text(creator);
+    this.ui.creator[creator ? 'show' : 'hide']();
+  }
+
+  /**
+   * @param {number} minimumSubTier
+   */
+  set tier(minimumSubTier) {
+    this.ui.tier.text(`Tier ${minimumSubTier}`);
+    this.ui.tier[minimumSubTier > 0 ? 'show' : 'hide']();
   }
 }
