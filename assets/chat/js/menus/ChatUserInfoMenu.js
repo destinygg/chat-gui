2b71bd7716ce95a7c6af6c48e4a2febeb3c9dac1
Farhad Jay <effejaye@gmail.com>
2023-04-06 10:56:22 -0700

    Set floating menu target as jQuery component

--- assets/chat/js/menus/ChatUserInfoMenu.js
+++ assets/chat/js/menus/ChatUserInfoMenu.js
@@ -6,7 +6,7 @@ import ChatMenuFloating from './ChatMenuFloating';
 
 export default class ChatUserInfoMenu extends ChatMenuFloating {
   constructor(ui, btn, chat) {
-    super(ui, btn, chat, '.toolbar');
+    super(ui, btn, chat, ui.find('.toolbar'));
 
     this.clickedNick = '';
     this.messageArray = [];
