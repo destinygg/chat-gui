ca953cd317067fa4a07e4ead7f68bacb4050dcd4
Farhad Jay <effejaye@gmail.com>
2023-05-20 00:20:01 -0700

    No longer load settings after connection
    The user isn't connected to chat until after chat history is loaded so
    that the welcome message appears after the history. If settings are
    loaded only after connecting, however, they won't apply to historical
    messages. This change allows a setup where settings can be loaded prior
    to history.

--- assets/chat/js/chat.js
+++ assets/chat/js/chat.js
@@ -1118,14 +1118,6 @@ class Chat {
 
   onME(data) {
     this.setUser(data);
-    if (data) {
-      // If is a logged in user.
-      this.loadSettings();
-      this.loadWhispers();
-    } else {
-      // If guest load default settings.
-      this.setSettings();
-    }
   }
 
   onOPEN() {
