30bee5deec1b464c1ac9b1182eba195f26287d78
KyrunaKJ <144215395+KyrunaKJ@users.noreply.github.com>
2024-11-23 23:56:01 -0500

    Update chat.js 'temp muted' sub/donate links
    Updated links to be relative

--- assets/chat/js/chat.js
+++ assets/chat/js/chat.js
@@ -1290,7 +1290,7 @@ class Chat {
       case 'muted': {
         this.mutedtimer.setTimer(data.muteTimeLeft);
         this.mutedtimer.startTimer();
-        const messageText = `You are temporarily muted! You can chat again ${this.mutedtimer.getReadableDuration()}. <a href="${this.config.dggOrigin}/subscribe">Subscribe</a>, or <a href="${this.config.dggOrigin}/donate">donate</a>, to remove the mute immediately.`;
+        const messageText = `You are temporarily muted! You can chat again ${this.mutedtimer.getReadableDuration()}. <a href="/subscribe">Subscribe</a>, or <a href="/donate">donate</a>, to remove the mute immediately.`;
 
         message = new ChatMessage(messageText, null, MessageTypes.ERROR, true);
         break;
