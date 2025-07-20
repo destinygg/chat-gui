6b1e02d546dc221720b776562a2b751d02765086
Farhad Jay <effejaye@gmail.com>
2025-07-20 16:58:34 -0700

    Implement service method to get user messages

--- assets/chat/js/services/UserMessageService.js
+++ assets/chat/js/services/UserMessageService.js
@@ -1,17 +1,53 @@
+// @ts-check
+
+import RustleSearchApiClient from './RustleSearchApiClient';
+
+/**
+ * @typedef {import('./RustleSearchApiClient').RustleSearchMessage} RustleSearchMessage
+ */
+
 /**
  * @typedef {Object} UserMessage
  * @property {string} username
  * @property {string} messageText
  * @property {number} timestamp
  */
+
 export default class UserMessageService {
+  /**
+   * @param {RustleSearchApiClient} [apiClient]
+   */
+  constructor(apiClient) {
+    this.apiClient = apiClient ?? new RustleSearchApiClient();
+  }
+
+  /**
+   * @param {Array<RustleSearchMessage>} messages
+   * @returns {Array<UserMessage>}
+   */
+  transformMessages(messages) {
+    return messages.map((message) => ({
+      username: message.username,
+      messageText: message.text,
+      timestamp: new Date(message.ts).getTime(),
+    }));
+  }
+
   /**
    * @param {string} username
    * @param {number} messageCount
    * @returns {Promise<Array<UserMessage>>}
    */
-  // eslint-disable-next-line no-unused-vars
   async getUserMessages(username, messageCount = 50) {
-    throw new Error('getUserMessages is not implemented');
+    let messages;
+    try {
+      messages = await this.apiClient.getLogs(username, 'Destinygg');
+    } catch (error) {
+      return [];
+    }
+
+    const transformedMessages = this.transformMessages(messages);
+
+    return transformedMessages.slice(0, messageCount);
   }
 }
