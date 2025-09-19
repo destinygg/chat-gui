3aabf224f98dbb4ce61620c8c514174522b1f066
Farhad Jay <effejaye@gmail.com>
2025-09-19 13:33:19 -0700

    Add the ability to remove a listener from EventEmitter

--- assets/chat/js/emitter.js
+++ assets/chat/js/emitter.js
@@ -20,6 +20,18 @@ class EventEmitter {
     }
     return false;
   }
+
+  off(name, fn) {
+    const listeners = this.listeners.get(name);
+    if (listeners && listeners.length) {
+      const index = listeners.indexOf(fn);
+      if (index > -1) {
+        listeners.splice(index, 1);
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
 export default EventEmitter;
